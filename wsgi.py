c91a4f58794718e49b4bc5de7da5d18504ed74b1
Yevhen Dudin <zdudin1@gmail.com>
2024-11-03 20:59:20 +0200

    added new files

--- wsgi.py
+++ wsgi.py
@@ -17,31 +17,24 @@ def get_films():
         }
     ]
 
-    @app.route('/')
-    def index():
-        def get_films():
-            return [
-                {
-                    'id': 1,
-                    'name': 'Harry Potter and the Philosopher\'s Stone',
-                    'relise_date': '2001-11-16'
-                },
-                {
-                    'id': 2,
-                    'name': 'Harry Potter and the Chamber of Secrets',
-                    'relise_date': '2002-11-16'
-                }
-            ]
 
-        return render_template('index.html', films=get_films())
+@app.route('/')
+@app.route('/hello')
+@app.route('/hello')
+def index():
+    films = get_films()
+    return render_template('hello.html', films=films)
 
-    @app.route('/about')
-    def about():
-        return render_template('about.html')
 
-    @app.route('/<string:name>')
-    def greating(name):
-        return f'<h1>Hello, {name.capitalize()}</h1>'
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
 
-    if __name__ == '__main__':
-        app.run()
+
+@app.route('/<string:name>')
+def greating(name):
+    return f'<h1>Hello, {name.capitalize()}</h1>'
+
+
+if __name__ == '__main__':
+    app.run()
